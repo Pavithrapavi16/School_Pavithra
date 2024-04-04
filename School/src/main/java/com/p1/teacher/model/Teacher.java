387c0179a280e9076cfea2b906836df98ba83579
Sowmya D <sowmya.d@tavant.com>
2024-04-04 16:05:06 +0530

    Merging

---
+++ School/src/main/java/com/p1/teacher/model/Teacher.java
@@ -0,0 +1,36 @@
+package com.p1.teacher.model;
+
+
+import com.example.SchoolManagementSchool.subject.model.Subject;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "teacher")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String dateOfBirth;
+    private String gender;
+    private String contactInformation;
+    private String address;
+
+    @ManyToMany(mappedBy = "teachers")
+    private Set<Subject> subjects = new HashSet<>();
+
+    // Constructors, getters, and setters
+}
